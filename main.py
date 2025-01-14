feff9cd3bfff210df72388c5fc5352175e1268d7
matheusssrc <matheusrossi2002@outlook.com>
2025-01-14 16:53:05 -0300

    The new version with user input

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-cpf = '69564912046'
-
 def calculate_first_digit(cpf: str, start_multiplier: int):
-    first_nine_digits= cpf[:9]
+    first_nine_digits = cpf[:9]
     formula_1_result = sum(
         int(digit) * multiplier 
         for digit, multiplier 
@@ -10,24 +8,36 @@ def calculate_first_digit(cpf: str, start_multiplier: int):
     formula_2_result = (formula_1_result * 10) % 11
     return str(formula_2_result) if formula_2_result <= 9 else "0"
 
-first_digit = calculate_first_digit(cpf, 10)
+def calculate_second_digit(cpf: str):
+    ten_digits = cpf[:10]
+    result_digit_2 = sum(
+        int(digit) * multiplier 
+        for digit, multiplier 
+        in zip(ten_digits, range(11, 1, -1))
+    )
+    digit_2 = (result_digit_2 * 10) % 11
+    return str(digit_2) if digit_2 <= 9 else "0"
 
-nine_digits = cpf[:9]
+def clean_cpf(cpf):
+    return cpf.replace('.', '').replace('-', '')
 
-ten_digits = nine_digits + first_digit
-counter_2 = 11
+def format_cpf(cpf):
+    return f'{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}'
 
-result_digit_2 = 0
-for digit in ten_digits:
-    result_digit_2 += (int(digit)) * counter_2
-    counter_2 -= 1
-digit_2 = (result_digit_2 *10) % 11
-digit_2 = digit_2 if digit_2 <= 9 else 0
+def is_valid_cpf(cpf):
+    if not cpf.isdigit() or len(cpf) != 11:
+        return False
+    
+    first_digit = calculate_first_digit(cpf, 10)
+    second_digit = calculate_second_digit(cpf[:9] + first_digit)
+    
+    cpf_generated = cpf[:9] + first_digit + second_digit
+    return cpf == cpf_generated
 
+cpf_input = input("Digite o CPF no formato ###.###.###-##: ")
+cpf_clean = clean_cpf(cpf_input)
 
-cpf_generated = f'{nine_digits}{first_digit}{digit_2}'
-
-if cpf == cpf_generated:
-    print(f'{cpf} é válido!')
+if is_valid_cpf(cpf_clean):
+    print(f'{format_cpf(cpf_clean)} é válido!')
 else:
-    print('CPF inválido!')
+    print(f'{format_cpf(cpf_clean)} CPF inválido!')
